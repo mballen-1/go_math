1d38700e88201a236cb80fa8918dcca6a72c1de2
Miguel <mballeng91@gmail.com>
2022-03-15 17:56:02 -0500

    Add v2

--- v2/arithmetic/arithmetic.go
+++ v2/arithmetic/arithmetic.go
@@ -1,5 +1,7 @@
 package arithmetic
 
+import "math"
+
 func Factorial(n int) int {
 	var f int = 1
 	for i := 2; i <= n; i++ {
@@ -16,4 +18,9 @@ func IsPrime(num int) bool {
 		}
 	}
 	return true
+}
+
+// CylinderVolume
+func CylinderVolume(r, h float64) float64 {
+	return math.Pi * math.Pow(r, 2.) * h
 }
---
+++ v2/geometry/geometry.go
@@ -0,0 +1,5 @@
+package geometry
+
+func Cube(s float32) float32 {
+	return s * s * s
+}
